4bc96de75d6df4ae3e56a36e7fb2791da7c4d7b1
Chien <chieniscool123@gmail.com>
2024-12-04 22:54:42 -0800

    shit don't work

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(word, num) {
+    if (num == 0) {
+        return ; 
+    }
+    let letter ;
 
+    for (let i = 0 ; i < num ; i ++ ) {
+        letter += word;
+    }
+    return letter;
 };
 
 // Do not edit below this line
