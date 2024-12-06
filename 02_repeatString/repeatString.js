21bd4a2e7ecc0e765eb6a92d1b8cd36148ef4c0b
Chien <chieniscool123@gmail.com>
2024-12-05 21:49:20 -0800

    Finished task 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,11 +1,18 @@
-const repeatString = function (word, times) {
-    if (times < 0) return "ERROR";
-    let string = "";
-    for (let i = 0; i < times; i++) {
-      string += word;
+const repeatString = function(word, num) {
+    if (num == 0) {
+        return '' ; 
     }
-    return string;
-  };
+    else if (num == -1) {
+        return 'ERROR';
+    }
+
+    let letter = "";
+
+    for (let i = 0 ; i < num ; i ++ ) {
+        letter += word;
+    }
+    return letter;
+};
 
 // Do not edit below this line
 module.exports = repeatString;
